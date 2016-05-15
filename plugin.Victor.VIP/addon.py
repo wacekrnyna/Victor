41784b354b89463de39b2d3c41612a8419836843
wacekrnyna <wacekrnyna@yahoo.com>
2016-05-15 23:31:01 +0200

    Update addon.py

--- plugin.Victor.VIP/addon.py
+++ plugin.Victor.VIP/addon.py
@@ -62,6 +62,7 @@ def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 def m3u2list(url):
+    url = 'http://przeklej.org/file/download/SYw2uU0HcajEt4qrXWnO'
     url = 'https://drive.google.com/uc?export=download&id=0B0PmlVIxygktR3dvSnByTTZtNFE'
     # url='http://api.moje-filmy.tk/nowa1.m3u8?cid=e940f0f2f3bff1f8812038acbad38dad'
     # url='http://api.moje-filmy.tk/nowa1.m3u8?cid=39ed77fae36199cac9dd68164ec6eba6'
