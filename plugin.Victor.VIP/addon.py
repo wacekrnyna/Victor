fb7a2c8a03d1e52ba13d87c0ddcd4210b7edc1ab
wacekrnyna <wacekrnyna@yahoo.com>
2017-02-26 13:03:11 +0100

    Update addon.py

--- plugin.Victor.VIP/addon.py
+++ plugin.Victor.VIP/addon.py
@@ -62,7 +62,7 @@ def build_url(query):
     return base_url + '?' + urllib.urlencode(query)
 
 def m3u2list(url):
-    url = 'https://www.easypaste.org/file/YoRmIk/tv.kopia.m3u?lang=pl'
+    url = 'http://dwiptv.cba.pl/iptv.m3u'
     # url = 'https://drive.google.com/uc?export=download&id=0B0PmlVIxygktR3dvSnByTTZtNFE'
     # url='http://api.moje-filmy.tk/nowa1.m3u8?cid=e940f0f2f3bff1f8812038acbad38dad'
     response = getUrl(getUrl(url))
