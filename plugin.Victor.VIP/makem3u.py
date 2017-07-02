9a83be9f285afdd761ce103b75fb13b69c2a5677
wacekrnyna <wacekrnyna@yahoo.com>
2017-07-02 12:26:16 +0200

    Update makem3u.py

--- plugin.Victor.VIP/makem3u.py
+++ plugin.Victor.VIP/makem3u.py
@@ -6,4 +6,4 @@ Created on Tue Jan 05 19:15:01 2016
 """
 
 print 'makem3u'
-#http://localhost/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22method%22:%22Addons.ExecuteAddon%22,%22params%22:{%22addonid%22:%22plugin.video.LivePolishTV%22,%22params%22:[%22mode=TEST%22]},%22id%22:1}
+#http://clientportal.link:8080/get.php?username=babak&password=babak1&type=m3u
